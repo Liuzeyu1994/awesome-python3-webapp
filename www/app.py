d377b66b396806eed1794a57688d052824f6bf25
Liuzeyu1994 <lzy9455@hotmail.com>
2017-02-01 11:49:34 -0500

    changed the format of asynchoronous IO app.py completed for section 2, require future modification

--- www/app.py
+++ www/app.py
@@ -1,18 +1,35 @@
-import logging; logging.basicConfig(level=logging.INFO)
-
-import asyncio, os, json, time
+# the logging module defines several functions and modules for journal processing
+# logging module can record errors and continue to execute after recording the errors
+import logging; 
+# set the level of loggging to INFO
+# hierarchy of journal：CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET
+logging.basicConfig(level=logging.INFO)
+# asyncio module for asynchornous IO
+import asyncio
+# os module for using functions of operating system
+import os
+# json module for trasforming python objects to json modules
+import json
+# time module for functions dealing with time
+import time
+# datetime module dealing with dates and time
 from datetime import datetime
-
+# aiohttp is the http framework based on asyncio
 from aiohttp import web
+
+'''
+from jinja2 import Environment, FileSystemLoader
+import orm
+from coroweb import add_routes, add_static
+'''
 
 def index(request):
 	return web.Response(body=b'<h1>Awesome</h1>')
 
-@asyncio.coroutine
-def init(loop):
+async def init(loop):
 	app = web.Application(loop=loop)
 	app.router.add_route('GET','/',index)
-	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+	srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info('server started at http://129.0.0.1:9000...')
 	return srv
 
